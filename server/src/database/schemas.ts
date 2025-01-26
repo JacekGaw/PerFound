e0d2390d092e872e3a7eb83d55175d2f6186bbc0
jacekgawlyta <jac.gawlyta@gmail.com>
2025-01-26 23:26:00 +0100

    Added account schema

--- server/src/database/schemas.ts
+++ server/src/database/schemas.ts
@@ -1,39 +1,67 @@
-import { boolean, pgTable, serial, timestamp, varchar, integer, decimal, pgEnum, text } from 'drizzle-orm/pg-core';
+import {
+  boolean,
+  pgTable,
+  serial,
+  timestamp,
+  varchar,
+  integer,
+  decimal,
+  pgEnum,
+  text,
+} from "drizzle-orm/pg-core";
 
-export const users = pgTable('users', {
-    id: serial('id').primaryKey(),
-    email: varchar('email', { length: 100 }).notNull().unique(),
-    password: varchar('password', { length: 200 }).notNull(),
-    createdAt: timestamp('createdAt').defaultNow(),
-    active: boolean('active').notNull().default(false),
-    name: varchar('name', { length: 20 }),
-    surname: varchar('surname', { length: 30 }),
-    phone: varchar('phone', { length: 20 }), // Changed to varchar
-    admin: boolean('admin').notNull().default(false)
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  email: varchar("email", { length: 100 }).notNull().unique(),
+  password: varchar("password", { length: 200 }).notNull(),
+  createdAt: timestamp("createdAt").defaultNow(),
+  active: boolean("active").notNull().default(false),
+  name: varchar("name", { length: 20 }),
+  surname: varchar("surname", { length: 30 }),
+  phone: varchar("phone", { length: 20 }), // Changed to varchar
+  admin: boolean("admin").notNull().default(false),
 });
 
-export const transactionTypeEnum = pgEnum('transactionTypes',["expense", "income"])
+export const transactionTypeEnum = pgEnum("transactionTypes", [
+  "expense",
+  "income",
+]);
 
-export const transactions = pgTable('transactions', {
-    id: serial('id').primaryKey(),
-    userId: integer('user_id')
-        .notNull()
-        .references(() => users.id),
-    amount: decimal('amount', { precision: 10, scale: 2 }).notNull(),
-    categoryId: integer('category_id')
-        .notNull()
-        .references(() => categories.id),
-    type: transactionTypeEnum('type').notNull(),
-    description: text('description'),
-    date: timestamp('date').notNull().defaultNow(),
-    createdAt: timestamp('createdAt').defaultNow()
+export const transactions = pgTable("transactions", {
+  id: serial("id").primaryKey(),
+  userId: integer("user_id")
+    .notNull()
+    .references(() => users.id),
+  accountId: integer("accountId")
+    .notNull()
+    .references(() => accounts.id),
+  amount: decimal("amount", { precision: 10, scale: 2 }).notNull(),
+  categoryId: integer("category_id")
+    .notNull()
+    .references(() => categories.id),
+  type: transactionTypeEnum("type").notNull(),
+  description: text("description"),
+  date: timestamp("date").notNull().defaultNow(),
+  createdAt: timestamp("createdAt").defaultNow(),
 });
 
+export const categories = pgTable("categories", {
+  id: serial("id").primaryKey(),
+  userId: integer("user_id")
+    .references(() => users.id)
+    .notNull(),
+  name: varchar("name", { length: 50 }).notNull(),
+  description: text("description"),
+});
 
-export const categories = pgTable('categories', {
-    id: serial('id').primaryKey(),
-    userId: integer('user_id')
-        .references(() => users.id).notNull(),
-    name: varchar('name', { length: 50 }).notNull(),
-    description: text('description'),
+export const accounts = pgTable("accounts", {
+  id: serial("id").primaryKey(),
+  userId: integer("userId")
+    .notNull()
+    .references(() => users.id),
+  name: varchar("name", { length: 100 }).notNull(),
+  type: varchar("type", { length: 50 }).notNull(),
+  balance: decimal("balance", { precision: 12, scale: 2 }).notNull(),
+  currencyCode: varchar("currencyCode", { length: 3 }).notNull(),
+  createdAt: timestamp("createdAt").defaultNow(),
 });
